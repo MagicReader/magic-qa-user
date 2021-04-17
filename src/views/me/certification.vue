a84161f9137b9967960f9af195e23d3db8c48e98
MagicReader <freedom_science@126.com>
2021-04-18 01:04:36 +0800

    修复BUG：身份认证图片上传的是本地url而不是图床url

--- src/views/me/certification.vue
+++ src/views/me/certification.vue
@@ -215,18 +215,27 @@ export default {
   },
   methods: {
     handleAvatarSuccess (res, file) {
-      this.uploadData.student_card_photo_src = URL.createObjectURL(file.raw)
-      console.log('上传图片', res.data)
+      console.log('上传图片', res)
+      switch (res.code) {
+        case 200:
+          this.uploadData.student_card_photo_src = URL.createObjectURL(file.raw)
+          this.uploadData.student_card_photo_src = res.data.url
+          console.log('图片地址：' + this.uploadData.student_card_photo_src)
+          break
+        default:
+          this.$message.error('上传图片失败!')
+          break
+      }
     },
     beforeAvatarUpload (file) {
       const isJPG = file.type === 'image/jpeg'
       const isLt2M = file.size / 1024 / 1024 < 2
 
       if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!')
+        this.$message.error('图片只能是 JPG 格式!')
       }
       if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!')
+        this.$message.error('图片大小不能超过 2MB!')
       }
       return isJPG && isLt2M
     },
